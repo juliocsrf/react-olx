aa3e01e08d65354e8a093f51298b8788fcd60214
Julio Cesar <juliocsrmf@gmail.com>
2021-02-22 21:11:19 -0300

    template básico

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 //import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import './App.css';
+
+import { Template } from './components/MainComponents';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
+
 import Routes from './Routes';
 
 const Page = (props) => {
@@ -10,7 +16,13 @@ const Page = (props) => {
 
     return (
         <BrowserRouter>
-            <Routes />
+            <Template>
+                <Header />
+
+                <Routes />
+
+                <Footer />
+            </Template>
         </BrowserRouter>
     )
 };
